144bda812034609bbd6aa0a9c7aff207de125b37
budaevdigital <budaev.digital@gmail.com>
2022-05-03 20:39:28 +0500

    add html and migrate base

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = 'django-insecure-x1&ml2!^pf22927pwazn@xss(8p=d9!2s*%uh5o*cza_4vv6yl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
