637a2c7e508f6e2901fe9669b6a729aea9dc2b9d
budaevdigital <budaev.digital@gmail.com>
2022-05-04 22:58:11 +0500

    create app 'users'

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,5 +18,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
+    # Django проверяет url-адреса сверху вниз,     
+	# нам нужно, чтобы Django сначала проверял адреса в приложении users
+	path('auth/', include('users.urls', namespace='users')),     
+	# Если какой-то URL не обнаружится в приложении users —     
+	# Django пойдёт искать его в django.contrib.auth     
+	path('auth/', include('django.contrib.auth.urls')),   
     path('admin/', admin.site.urls),
 ]
