a62fa8c17aa5462aa31444feda23ab8054be1171
budaevdigital <budaev.digital@gmail.com>
2022-05-05 01:35:18 +0500

    create new app 'users'

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
     'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
