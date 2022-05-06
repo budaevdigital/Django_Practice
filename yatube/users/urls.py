2c4d920bd21172d71b220f8cbd26d4f3532f2a48
budaevdigital <budaev.digital@gmail.com>
2022-05-06 18:35:56 +0500

    add signup template

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,14 +1,20 @@
 # Импортируем из приложения django.contrib.auth нужный view-класс 
 from django.contrib.auth.views import LogoutView  
-from django.urls import path  
+from django.urls import path
+from . import views
 
 app_name = 'users'
 
 urlpatterns = [     
 	path('logout/',       
-        # Прямо в описании обработчика укажем шаблон,        
-        # который должен применяться для отображения возвращаемой страницы.
-        # Да, во view-классах так можно!
-        LogoutView.as_view(template_name='users/logged_out.html'),
-        name='logout'), 
+            # Прямо в описании обработчика укажем шаблон,        
+            # который должен применяться для отображения возвращаемой страницы.
+            # Да, во view-классах так можно!
+            LogoutView.as_view(template_name='users/logged_out.html'),
+            name='logout'),
+        
+    path('signup/',
+        # Полный адрес страницы регистрации - auth/signup/,     
+	    # но префикс auth/ обрабатывется в головном urls.py 
+        views.SignUp.as_view(), name='signup')
 ]
--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+# Импортируем функцию reverse_lazy, чтобы получить URL по 
+# параметрам функции path() 
+from django.urls import reverse_lazy
+
+# Импортируем свой класс формы, чтобы сослаться на него
+# во view-классе
+from .forms import CreationForm
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    # В случае успешной регистрации, перенаправляем
+    # по 'namespace:name'
+    success_url = reverse_lazy('post:index')
+    template_name = 'users/signup.html'
