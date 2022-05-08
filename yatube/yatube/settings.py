6b417754cd94a10e0c669fccaf3bfd8c54c907cc
budaevdigital <budaev.digital@gmail.com>
2022-05-09 00:58:38 +0500

    add favicon

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -107,7 +107,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
-# LOGOUT_REDIRECT_URL = 'posts:index'
+LOGOUT_REDIRECT_URL = 'posts:index'
+
+
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
