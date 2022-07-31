41672de28d08a161e873325b94de12933b7ae447
budaevdigital <budaev.digital@gmail.com>
2022-07-31 22:26:26 +0000

    добавлен мониторинг в Sentry

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -9,12 +9,30 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+from dotenv import load_dotenv
 import os
 from pathlib import Path
 from datetime import timedelta
-from dotenv import load_dotenv
+
 load_dotenv()
+
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_DNS'),
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
