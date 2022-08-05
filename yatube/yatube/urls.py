7edbffb5c9ea1817a69db380d62e188d9edab3c8
budaevdigital <budaev.digital@gmail.com>
2022-08-05 19:01:56 +0000

    template fix

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
 ]
 
 handler404 = 'core.views.page_not_found'
-# handler500 = 'core.views.server_error'
+handler500 = 'core.views.server_error'
 handler403 = 'core.views.permission_denied'
 
 schema_view = get_schema_view(
