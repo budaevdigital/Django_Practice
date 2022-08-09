efcfc266134a998d280a8d08936ee453a92d9c40
Dmitry Budaev <budaev.digital@gmail.com>
2022-08-10 00:36:18 +0500

    Настроил редирект с www

--- init-user-db.sh
+++ init-user-db.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-./yatube/manage.py collectstatic --noinput
-
 # psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB_NAME" <<-EOSQL
 psql -v ON_ERROR_STOP=1 -U postgres <<-EOSQL
 	CREATE USER yatube_user WITH ENCRYPTED PASSWORD '$POSTGRES_PASSWORD';
